7cc147e0e58ee250bba3827d457dd54fb2d8c6aa
Nalipp <nateplipp@gmail.com>
2022-12-09 15:28:26 -0800

    Get button touch events working on mobile

--- public/button.js
+++ public/button.js
@@ -6,6 +6,26 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach(buttonEle => applyButtonPressEffect(buttonEle));
 
 function applyButtonPressEffect(buttonEle) {
-  buttonEle.addEventListener("mousedown", () => buttonEle.style["box-shadow"] = "none" );
-  buttonEle.addEventListener("mouseup", () => buttonEle.style["box-shadow"] = "5px 5px #F02FA3" );
+  buttonEle.addEventListener("mousedown", () => {
+    buttonEle.style["box-shadow"] = "none" 
+    buttonEle.style["top"] = "5px";
+    buttonEle.style["left"] = "5px";
+  });
+  buttonEle.addEventListener("mouseup", () => {
+    buttonEle.style["box-shadow"] = "5px 5px #F02FA3";
+    buttonEle.style["top"] = "0";
+    buttonEle.style["left"] = "0";
+  });
+
+  // mobile
+  buttonEle.addEventListener("touchstart", () => {
+    buttonEle.style["box-shadow"] = "none" 
+    buttonEle.style["top"] = "5px";
+    buttonEle.style["left"] = "5px";
+  });
+  buttonEle.addEventListener("touchend", () => {
+    buttonEle.style["box-shadow"] = "5px 5px #F02FA3";
+    buttonEle.style["top"] = "0";
+    buttonEle.style["left"] = "0";
+  });
 }
