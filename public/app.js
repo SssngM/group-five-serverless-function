70fcfdb3dc4c62fa1fbe8ffb1f236be35db1ef5e
Nalipp <nateplipp@gmail.com>
2022-12-08 21:18:17 -0800

    Add clipboard funtionality

--- public/app.js
+++ public/app.js
@@ -162,3 +162,23 @@ validateForm();
 // the following prevents checkbox state from persisting after form submissions
 const inputs = document.querySelectorAll('input');
 inputs.forEach(input => input.autocomplete = 'off');
+
+
+/* ******************************************************************************** */
+/* clipboard funtionality */
+/* ******************************************************************************** */
+
+function myFunction() {
+  const copyText = document.getElementById("clipboard-input");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  
+  const clipboardBtn = document.getElementById("clipboard-btn");
+  clipboardBtn.innerHTML = "Copied";
+}
+
+function outFunc() {
+  const clipboardBtn = document.getElementById("clipboard-btn");
+  clipboardBtn.innerHTML = "Copy URL";
+}
