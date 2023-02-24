d6239efe95f842499e02162ac9b27aa94e082a4a
SssngM <rarang222@gmail.com>
2023-02-24 11:51:39 -0800

    update button and font size

--- public/betaform.js
+++ public/betaform.js
@@ -69,11 +69,6 @@ function processQuestion2(curTarget) {
   function markCheckbox(checkbox) {
     question2State.push(checkbox);
 
-    // if (question2State.length > 3) {
-    //   const checkbox = question2State.shift();
-    //   checkbox.checked = false;
-    // }
-
     question2State.forEach((checkbox) => {
       checkbox.checked = true;
     });
@@ -104,11 +99,6 @@ function processQuestion3(curTarget) {
   function markCheckbox(checkbox) {
     question3State.push(checkbox);
 
-    // if (question3State.length > 3) {
-    //   const checkbox = question3State.shift();
-    //   checkbox.checked = false;
-    // }
-
     question3State.forEach((checkbox) => {
       checkbox.checked = true;
     });
@@ -128,9 +118,6 @@ function validateForm() {
   const $emailValue = document.querySelector("input[name=email]").value;
 
   if (
-    // question1State.length === 2
-    //   && question2State.length === 3
-    //   && question3State.length === 3
     $emailValue !== '') {
     $validationMessage.style["visibility"] = "hidden";
     $btn2.style["pointer-events"] = "inherit";
